45301426a1cb41800a74c3ccd89acc226dd67048
Faisal <pros3sa@gmail.com>
2025-05-14 10:50:17 +0000

    refactor: convert main.js to main.ts

---
+++ src/advanceSettings.ts
@@ -0,0 +1,137 @@
+'use strict'
+
+import General from "./general.js";
+
+class AdvanceSettings extends General {
+    constructor () {
+        super();
+
+    }
+
+    markup (component: { name: string; numOfLights: number; autoOn: string; autoOff: string; }) {
+        const {name, numOfLights, autoOn, autoOff} = component;
+        return `
+            <section class="component_summary">
+                <div>
+                    <p class"component_name">${this.capFirstLetter(name)} lights</p>
+                    <p class="number_of_lights">${numOfLights}</p>
+                </div>
+                <div>
+
+                    <p class="auto_on">
+                        <span>Automatic turn on:</span>
+                        <span>${autoOn}</span>
+                    </p>
+                    <p class="auto_off">
+                        <span>Automatic turn off:</span>
+                        <span>${autoOff}</span>
+                    </p>
+                </div>
+            </section>
+            <section class="customization">
+                <div class="edit">
+                    <p>Customize</p>
+                    <button class="customization-btn">
+                        <img src="./assets/svgs/edit.svg" alt="customize settings svg icon">
+                    </button>
+                </div>
+                <section class="customization-details hidden">
+                    <div>
+                        <h4>Automatic on/off settings</h4>
+                        <div class="defaultOn">
+                            <label for="">Turn on</label>
+                            <input type="time" name="autoOnTime" id="autoOnTime">
+                            <div>
+                                <button class="defaultOn-okay">Okay</button>
+                                <button class="defaultOn-cancel">Cancel</button>
+                            </div>
+                        </div>
+                        <div class="defaultOff">
+                            <label for="">Go off</label>
+                            <input type="time" name="autoOffTime" id="autoOffTime">
+                            <div>
+                                <button class="defaultOff-okay">Okay</button>
+                                <button class="defaultOff-cancel">Cancel</button>
+                            </div>
+                        </div>
+
+                    </div>
+                </section>
+                <section class="summary">
+                    <h3>Summary</h3>
+                    <div class="chart-container">
+                        <canvas id="myChart"></canvas>
+                    </div>
+                </section>
+            </section>
+        `
+    }
+
+    getSelectedComponent (componentName: string | undefined) {
+        if (!componentName) return this.componentsData;
+        const component = this.componentsData[componentName.toLowerCase()];
+        return component;
+    }
+
+    getSelectedSettings (componentName: string | undefined) {
+        return this.markup(this.getSelectedComponent(componentName));
+
+    }
+
+    setNewData (component: string, key: 'autoOn' | 'autoOff', data: string) {
+        const selectedComponent = this.componentsData[component.toLowerCase()];
+        return selectedComponent[key] = data;
+    }
+
+    capFirstLetter (word: string) {
+        return word.replace(word.at(0)!, word.at(0)!.toUpperCase())
+    }
+
+    getObjectDetails() {
+        return this;
+    }
+
+    formatTime (time: string): Date {
+        const [hour, min] = time.split(':');
+
+        const dailyAlarmTime = new Date();
+        dailyAlarmTime.setHours(parseInt(hour));
+        dailyAlarmTime.setMinutes(parseInt(min));
+        dailyAlarmTime.setSeconds(0);
+
+        return dailyAlarmTime;
+    };
+
+    async timer (time: Date, message: boolean, component: string): Promise<boolean> {
+        return new Promise ((resolve, reject) => {
+            const checkAndTriggerAlarm = () => {
+                const now = new Date();
+                console.log(time, now);
+                if (
+                    now.getHours() === time.getHours() &&
+                    now.getMinutes() === time.getMinutes() &&
+                    now.getSeconds() === time.getSeconds()
+                ) {
+                    console.log(message);
+                    this.isLightOff = false;
+                    this.componentsData[component].isLightOff = message;
+                    resolve (this.componentsData[component].isLightOff)
+                }
+            }
+
+            // Check every second
+            setInterval(checkAndTriggerAlarm, 1000);
+
+        })
+    }
+
+    async automateLight (time: string, component: string): Promise<boolean> {
+        const formattedTime = this.formatTime(time);
+        return await this.timer(formattedTime, true, component);
+
+    }
+
+
+}
+
+export default AdvanceSettings;
